bb112157f3d4ed2c80f5e90dc084a8715779d7fc
Ghto007 <144988529+Ghto007@users.noreply.github.com>
2024-11-27 12:39:13 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -52,7 +52,24 @@ resource "aws_instance" "web_instance" {
   ami           = "ami-0166fe664262f664c"
   instance_type = "t2.micro"
   security_groups = ["web_app"]
- 
+ user_data = <<-EOF
+  #!/bin/bash
+  # Встановлення Docker
+  curl -fsSL https://get.docker.com -o get-docker.sh
+  sudo sh get-docker.sh
+  sudo usermod -aG docker ec2-user  # Додавання користувача ec2-user в групу docker
+
+  # Перезавантаження групи для користувача
+  newgrp docker
+
+  # Завантаження і запуск контейнера
+  docker pull ghto007/aws:latest
+  docker run -id ghto007/aws:latest
+
+
+  # Перевірка, чи запущено Docker
+  docker ps
+  EOF
 
   tags = {
     Name = "webapp_instance"
